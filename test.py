e559154337b54602a62708493567cb255078f3ba
lajan <aiswarialajan124@gmail.com>
2026-05-08 13:15:07 +0100

    Used test.py to find error - a missing package 'cryptography'

--- test.py
+++ test.py
@@ -1,10 +1,12 @@
 print("start")
-import mysql.connector
-print("imported")
-conn = mysql.connector.connect(
-    host="localhost",
-    user="root",
-    password="root",
-    database="appdbproj"
-)
-print("connected")
+import pymysql
+print("imported ok")
+try:
+    conn = pymysql.connect(host='localhost',user='root',password='root',database='appdbproj')
+    print('connected ok')
+    c = conn.cursor()
+    c.execute('SHOW TABLES')
+    print(c.fetchall())
+except BaseException as e:
+    print("ERROR:", type(e).__name__, e)
+print("done")
